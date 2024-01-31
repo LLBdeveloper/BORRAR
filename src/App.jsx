dc0a5f1748d2cc104825ff3e2bad40e06ebb0864
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-01-31 20:25:45 -0300

    start repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MercadoLibre from './components/MercadoLibre'
+
+function App() {
+
+  return (
+    <>
+      <MercadoLibre/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MercadoLibre.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+
+function MercadoLibre() {
+    
+    useEffect(()=> {
+        fetch('',{})
+    },[])
+
+
+
+    return (
+        <div>
+            <h1>MercadoLibre</h1>
+        </div>
+    )
+}
+
+export default MercadoLibre
